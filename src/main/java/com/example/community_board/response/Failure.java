bea17c4cae9a83c2ea609e66c393e96b8c4d14bb
chrkb1569 <chrkb1569@naver.com>
2022-09-14 16:42:21 +0900

    feat : Add Response Files

--- src/main/java/com/example/community_board/response/Failure.java
+++ src/main/java/com/example/community_board/response/Failure.java
@@ -1,4 +1,10 @@
 package com.example.community_board.response;
 
-public class Failure {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Failure implements Result{
+    private String message;
 }
