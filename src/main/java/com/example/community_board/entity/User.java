6dedca7da2bddf941cddfb88bd416b5062193f95
chrkb1569 <chrkb1569@naver.com>
2022-09-17 15:04:38 +0900

    feat : Add User Entity & Controller & Service & Repository

--- src/main/java/com/example/community_board/entity/User.java
+++ src/main/java/com/example/community_board/entity/User.java
@@ -1,16 +1,36 @@
 package com.example.community_board.entity;
 
-import lombok.Generated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
 }
--- src/main/java/com/example/community_board/service/UserService.java
+++ src/main/java/com/example/community_board/service/UserService.java
@@ -1,4 +1,14 @@
 package com.example.community_board.service;
 
+import com.example.community_board.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+
 }
